61a1643005304986426ebeb2615ce29f1a722e43
Aodhan Roche <aodhanroche@Aodhans-MacBook-Pro.local>
2023-01-11 13:08:36 -0800

    second-attempt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,9 +9,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/') 
+      .then(res => {
+        setCharacters(res.data);
+      })
+  }, [])
+
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map((dev) => {
+          return (<p>{dev.name}</p>)
+        })
+      }
     </div>
   );
 }
